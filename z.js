fe0a16d40b8a71cff6fafbbdb2a568ba723207fc
lvdev <lov3r.dev@gmail.com>
2024-09-27 16:40:15 +0100

    adding DOM selectors to variables

---
+++ z.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
